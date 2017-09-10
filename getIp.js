6c3f7f476d00ae27f318c0a43f2ea532d87845c8
Jose R. Padron <Jose.Padron@ge.com>
2017-09-10 00:13:44 -0400

    Add timer for getting ip

--- getIp.js
+++ getIp.js
@@ -2,12 +2,19 @@ var Firebase = require("firebase");
 var myDataRef = new Firebase('https://flickering-torch-9611.firebaseio.com/');
 var os = require('os');
 
+function bye()
+{
+  process.exit(0);
+}
+
 //Set server ip
 myDataRef.child("server").set({
   server_ip: ip_address('wlan0')
 });
 
-process.exit(0);
+setInterval(function () {
+   bye(bus);
+}, 2000);
 
 function ip_address(interface) {
   var items = os.networkInterfaces()[interface] || [];
